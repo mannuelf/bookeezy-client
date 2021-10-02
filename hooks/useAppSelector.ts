bbcee535618ec3ea5b1c366753e837ae68b61189
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-10-02 17:42:10 +0200

    redux: created starter store, export RootState and AppDispatch

--- hooks/useAppSelector.ts
+++ hooks/useAppSelector.ts
@@ -1,3 +1,4 @@
 import { TypedUseSelectorHook, useSelector } from 'react-redux';
-import type { RootSate } from '../store';
+import type { RootState } from '../store';
+
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
--- store.ts
+++ store.ts
@@ -4,5 +4,5 @@ export const store = configureStore({
   reducer: {},
 });
 
-export type RootSate = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
