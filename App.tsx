852bd083e45af21b2fbf7e34303e8eedbcf8a0b4
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-10-08 20:50:58 +0200

    register page connected to backend

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Text } from 'react-native';
+import { NativeBaseProvider } from 'native-base';
+import { Provider, createClient } from 'urql';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import { GRAPHQL } from './constants/';
+
+const graphqlClient = createClient({
+  url: GRAPHQL.endpoint,
+  fetchOptions: {
+    credentials: 'include',
+  },
+});
 
 export default function App(): JSX.Element {
   const isLoadingComplete = useCachedResources();
@@ -18,10 +29,14 @@ export default function App(): JSX.Element {
     );
   } else {
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
+      <Provider value={graphqlClient}>
+        <SafeAreaProvider>
+          <NativeBaseProvider>
+            <Navigation colorScheme={colorScheme} />
+            <StatusBar />
+          </NativeBaseProvider>
+        </SafeAreaProvider>
+      </Provider>
     );
   }
 }
--- components/register/Register.tsx
+++ components/register/Register.tsx
@@ -1,52 +1,108 @@
-import React, { InputHTMLAttributes } from 'react';
-import { MaterialCommunityIcons, Ionicons } from '@expo/vector-icons';
+import React from 'react';
 import {
-  NativeBaseProvider,
   Box,
+  NativeBaseProvider,
   Heading,
   VStack,
   FormControl,
   Input,
   Button,
 } from 'native-base';
-import { InputField } from '../InputField';
+import { useMutation } from 'urql';
 
-export default function Register(): JSX.Element {
-  const onSubmit = () => {
-    console.log('Submitted');
+interface IRegister {
+  email: string;
+  password: string;
+  errors: string;
+}
+
+const REGISTER_MUTATION = `
+  mutation Register($email: String!, $password: String!) {
+    register(options: { email: $email, password: $password }) {
+      user {
+        email
+        password
+      }
+    }
+  }`;
+
+const Register: React.FC = () => {
+  const [, register] = useMutation(REGISTER_MUTATION);
+  const [formData, setData] = React.useState<IRegister>({} as IRegister);
+  const [errors, setErrors] = React.useState<IRegister>({} as IRegister);
+
+  const validate = async (): Promise<boolean> => {
+    for (let key in formData) {
+      if (key === undefined) {
+        setErrors({
+          ...errors,
+          key: `${key} is required`,
+        });
+        return false;
+      } else if (key.length < 3) {
+        setErrors({
+          ...errors,
+          key: `${key} is required`,
+        });
+        return false;
+      }
+    }
+
+    await register(formData);
+    return true;
+  };
+
+  const onSubmit = async () => {
+    (await validate())
+      ? console.log('Submitted', formData)
+      : console.log('Validation failed');
   };
 
   return (
     <NativeBaseProvider>
-      <Box safeArea flex={1} p="2" w="100%" mx="auto" py="8">
+      <Box safeArea flex={1} p="0" w="100%" mx="auto" py="0">
         <Heading size="lg" color="coolGray.800" fontWeight="600">
           Welcome
         </Heading>
         <Heading mt="1" color="coolGray.600" fontWeight="medium" size="xs">
-          Sign up today to start uploading and sharing books.
+          Sign up to continue!
         </Heading>
 
         <VStack space={3} mt="5">
-          <InputField
-            id="firstName"
-            name="firstName"
-            placeholder="First name"
-            type="text"
-            label="First Name"
-          />
-          <FormControl>
+          <FormControl isRequired isInvalid={'email' in errors}>
+            <FormControl.Label
+              _text={{ color: 'muted.700', fontSize: 'xs', fontWeight: 500 }}>
+              Email
+            </FormControl.Label>
+            <Input onChangeText={(value) => setData({ ...formData, email: value })} />
+            {'email' in errors ? (
+              <FormControl.HelperText _text={{ fontSize: 'xs' }}>
+                Email should contain at least 3 character.
+              </FormControl.HelperText>
+            ) : (
+              <FormControl.ErrorMessage _text={{ fontSize: 'xs' }}>
+                Error Email
+              </FormControl.ErrorMessage>
+            )}
+          </FormControl>
+          <FormControl isRequired isInvalid={'password' in errors}>
             <FormControl.Label
               _text={{ color: 'muted.700', fontSize: 'xs', fontWeight: 500 }}>
               Password
             </FormControl.Label>
-            <Input type="password" />
-          </FormControl>
-          <FormControl>
-            <FormControl.Label
-              _text={{ color: 'muted.700', fontSize: 'xs', fontWeight: 500 }}>
-              Confirm Password
-            </FormControl.Label>
-            <Input type="password" />
+            <Input
+              onChangeText={(value) => setData({ ...formData, password: value })}
+              type="password"
+            />
+            {'password' in errors ? (
+              <FormControl.HelperText _text={{ fontSize: 'xs' }}>
+                Error with password
+              </FormControl.HelperText>
+            ) : (
+              <FormControl.ErrorMessage _text={{ fontSize: 'xs' }}>
+                Error Password
+              </FormControl.ErrorMessage>
+            )}
           </FormControl>
           <Button
             onPress={onSubmit}
@@ -59,4 +115,6 @@ export default function Register(): JSX.Element {
       </Box>
     </NativeBaseProvider>
   );
-}
+};
+
+export default Register;
